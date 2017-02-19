0e412f798d5539a2709a4f70469af65d3fd8702c
Duncan Sterken <duncan.sterken123@gmail.com>
2017-02-19 10:34:28 +0100

    Update Config.java

--- main/java/me/duncte123/blazemedia/utils/Config.java
+++ main/java/me/duncte123/blazemedia/utils/Config.java
@@ -3,5 +3,5 @@ package me.duncte123.blazemedia.utils;
 public class Config {
 	public static final String token = "BotToken";
 	public static final String prefix = "%";
-	public static final String version = "2.0";
+	public static final String version = "2.1";
 }
